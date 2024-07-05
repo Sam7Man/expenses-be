f49b0073e8bf1856390bbae860709241a12adb4b
Samuel Manalu <samman.r007@gmail.com>
2024-07-05 18:29:28 +0700

    Refactor auth middleware to handle Bearer tokens

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,14 +1,36 @@
 const jwt = require('jsonwebtoken');
 
+// # custom header
+// module.exports = (req, res, next) => {
+//     const token = req.header('x-auth-token');
+//     if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+
+//     try {
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.user = decoded.user;
+//         next();
+//     } catch (err) {
+//         res.status(401).json({ message: 'Token is not valid' });
+//     }
+// };
+
+
+// Bearer Token header
 module.exports = (req, res, next) => {
-    const token = req.header('x-auth-token');
-    if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Authorization denied' });
+    }
+
+    // Extract token from Authorization header
+    const token = authHeader.split(' ')[1];
 
     try {
+        // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
         next();
     } catch (err) {
-        res.status(401).json({ message: 'Token is not valid' });
+        res.status(401).json({ message: 'Invalid token' });
     }
 };
